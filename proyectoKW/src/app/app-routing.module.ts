174262ab053c24796569e9e9443c329cf52e7a2e
Yadimir <yadimir.cova@gmail.com>
2023-08-29 17:21:20 -0600

    se modifico el app.routing.module.ts, tambien app.module.ts para que los vinculos funcionaran con component.html donde agregamos router-outlet, tambien ya enlaza a login y notaNueva.  por ultimo agregamos los imputs para las notas

--- proyectoKW/src/app/app-routing.module.ts
+++ proyectoKW/src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
+import { ModuleWithProviders } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
+
 import { NotasNuevasComponent } from './components/notasNuevas/notasNuevas/notasNuevas.component';
-import { LogicalOperator } from 'typescript';
 import { LoginComponent } from './components/login/login/login.component';
+import { ErrorComponent } from './components/error/error/error.component';
 
 const routes: Routes = [
   {path:'',component:LoginComponent},
-  {path:'notaNueva', component:NotasNuevasComponent}
+  {path:'notaNueva', component:NotasNuevasComponent},
+  {path:'**', component: ErrorComponent}
 
 ];
 
@@ -15,3 +19,7 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
+export const appRoutingProviders: any[] = [];
+export const routing: ModuleWithProviders<any> = RouterModule.forRoot(routes);
--- proyectoKW/src/app/app.module.ts
+++ proyectoKW/src/app/app.module.ts
@@ -1,22 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { routing,appRoutingProviders } from './app-routing.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NotasNuevasComponent } from './components/notasNuevas/notasNuevas/notasNuevas.component';
 import { LoginComponent } from './components/login/login/login.component';
+import { ErrorComponent } from './components/error/error/error.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NotasNuevasComponent,
-    LoginComponent
+    LoginComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    routing
   ],
-  providers: [],
+  providers: [
+    appRoutingProviders
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
