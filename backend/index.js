7a593563ee97f4031e65b256114eeca34835cc9c
Yadimir <yadimir.cova@gmail.com>
2023-10-13 11:47:05 -0600

    dentro de backend en el file index se agrego el use strict, var mongoose, mongoose promise, mongoose.conect, .then y .catch

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+'use strict'
+var mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/proyKingWash')
+    .then(()=>{
+    console.log("conexion a la base de datos establecida...satisfactoriamente");
+    })   
+    .catch(err => console.log(err));
+
+/*
+    mongoose.connect('mongodb://localhost:27017/proyKingWash', { useNewUrlParser:true, useUnifiedTopology: true})
+    .then(()=>{
+        console.log("conexion a la base de datos establecida...satisfactoriamente");
+    
+        //Creasion de Servidor
+        app.listen(port,()=>{
+            console.log("servidor corriendo de forma correcta en la url: localhost:3700")
+        });
+    })
+    .catch(err => console.log(err));  */
