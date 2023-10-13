0f3ad9688d9c8eeebf1e95a4ef126586b5200931
Yadimir <yadimir.cova@gmail.com>
2023-10-13 12:42:18 -0600

    dentro del folder backend se genero el app.lisener

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,17 @@
 'use strict'
 var mongoose = require('mongoose');
 var app = require('./app');
-var port=3700;
+var port=3700; 
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/proyKingWash')
     .then(()=>{
     console.log("conexion a la base de datos establecida...satisfactoriamente");
         //creacion de servidor
+
+        app.listen(port,()=> {
+            console.log("servidor corriendo correctamente en la url: localhost:3700");
+        });
     })   
     .catch(err => console.log(err));
 
