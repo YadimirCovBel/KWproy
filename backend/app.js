046c79a5e84376503bc5b0de1caff46f20f7672d
Yadimir <yadimir.cova@gmail.com>
2024-01-30 18:34:05 -0600

    dentro de app.js se cambio app.get('/test por app.post('/test

--- backend/app.js
+++ backend/app.js
@@ -23,7 +23,7 @@ app.get('/', (req,res)=> {
     );
 });
 
-app.get('/test', (req,res)=> {
+app.post('/test', (req,res)=> {
     res.status(200).send({
         message: "hola munedo desde mi aAPI de NodeJS"
     });
