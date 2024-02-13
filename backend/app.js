984253617015ebe73a664fb995d125faf7e4ddc5
Yadimir <yadimir.cova@gmail.com>
2024-02-13 15:14:31 -0600

    se eliminaron las rutas dentro del folder back end en el folder node_modules editando el file app.js

--- backend/app.js
+++ backend/app.js
@@ -17,25 +17,7 @@ app.use(bodyParser.json());
 
 //rutas
 
-app.get('/', (req,res)=> {
-    res.status(200).send(
-        "<h1>Pagina de inicio <h1>"
-    );
-});
 
-app.post('/test/:id', (req,res)=> {
-    console.log(req.body.Nombre);
-    //request.body: Contiene los parámetros que le pasemos por el cuerpo de la petición.
-    console.log(req.query.web);
-    //request.query: Contiene los parámetros que le pasemos por la URL desde desde NodeJS.
-    console.log(req.params.id);
-    //request.params: Contiene los parámetros que le pasemos por la URL.
-
-
-    res.status(200).send({
-        message: "hola munedo desde mi API de NodeJS"
-    });
-});
 //req son los datos que puedes estar enviando  desde el cliente o respuestas que haga 
 //res es una respuesta que sera enviada
 
